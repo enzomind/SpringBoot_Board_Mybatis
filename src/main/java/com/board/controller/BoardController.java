16bbfb2e16b3c5fdaa50b71545db98f098e518bf
Logan <redkal19@gmail.com>
2022-09-26 17:08:49 +0900

    v0.5 프론트 및 CRUD 구현 진행

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -1,10 +1,14 @@
 package com.board.controller;
 
+import com.board.domain.BoardDTO;
 import com.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,17 +17,36 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping(value = "/board/write")
-    public String openBoardWrite(Model model) {
+    public String openBoardWrite(@RequestParam(value="idx", required = false) Long idx, Model model) {
 
-        String title="제목 파람";
-        String content="테스트 내용 파람";
-        String writer="작성테스터 파람";
-
-        model.addAttribute("titleParam", title);
-        model.addAttribute("contentParam", content);
-        model.addAttribute("writerParam", writer);
+        if(idx==null) { //idx값 없을 땐 리스트가져오고
+            model.addAttribute("board", new BoardDTO());
+        } else { //있으면 상세화면으로
+            BoardDTO boardDTO = boardService.getBoardDetail(idx);
+            if(boardDTO == null) { //예외처리
+                return "redirect:/board/list";
+            }
+            model.addAttribute("board", boardDTO); //상세에 보여줄 DTO값을 말아오기
+        }
 
         return "board/write";
+    }
+
+
+    @PostMapping(value = "/board/register")
+    public String registerBoard(final BoardDTO params){
+        try{
+            boolean isRegistered = boardService.registerBoard(params);
+            if(isRegistered == false) {
+
+            }
+        } catch (DataAccessException e) {
+
+        } catch (Exception e) {
+
+        }
+
+        return "redirect:/board/list";
     }
 
 }
